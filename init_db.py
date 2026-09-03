a36a1c66c718997a76ed0ab74dc4c9188d827202
yonisalazar47-lang <yoni.salazar47@gmail.com>
2026-09-03 15:18:01 -0300

    Add files via upload

--- init_db.py
+++ init_db.py
@@ -4,20 +4,15 @@ def init_db():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
-    # Tabla de Productos con 4 precios
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS productos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT NOT NULL,
             stock INTEGER NOT NULL,
-            precio_1 REAL DEFAULT 0.0,
-            precio_2 REAL DEFAULT 0.0,
-            precio_3 REAL DEFAULT 0.0,
-            precio_4 REAL DEFAULT 0.0
+            precio REAL DEFAULT 0.0
         )
     ''')
 
-    # Tabla de Pedidos
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS pedidos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
